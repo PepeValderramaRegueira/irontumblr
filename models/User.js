4eedc8dc66e96c4acf9c3b96d9c56669d9777615
Julio <juhume.exe@gmail.com>
2019-09-19 12:34:09 +0200

    first commit

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const SALT_WORK_FACTOR = 10;
+
+const PASSWORD_PATTERN = /(?=.\d)(?=.[a-z])(?=.*[A-Z]).{6,}/;
+const EMAIL_PATTERN = /^(([^<>()\[\]\.,;:\s@\“]+(\.[^<>()\[\]\.,;:\s@\“]+)*)|(\“.+\“))@(([^<>()[\]\.,;:\s@\“]+\.)+[^<>()[\]\.,;:\s@\“]{2,})$/i;
+
+const userSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    password: {
+      type: String,
+      required: true,
+      match: [PASSWORD_PATTERN, "this is not a correct password"]
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+      match: [EMAIL_PATTERN, "this is not a correct email"]
+    },
+    photo: {
+      type: String
+    },
+    validationCode: { type: String, unique: true },
+    active: false
+  },
+  {
+    timestamps: true
+  }
+);
+
+userSchema.methods.checkPassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
